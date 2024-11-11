a2585d474ab3511b48a4b5a953ee3ba560b75cfd
Max <max.lei@bk.ru>
2024-11-11 13:17:03 +0300

    переписал с activity на фрагменты и доработал дз 7

--- app/src/main/java/com/example/myapplication/ui/joke_list/JokesActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/joke_list/JokesActivity.kt
@@ -1,24 +1,28 @@
 package com.example.myapplication.ui.joke_list
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.myapplication.R
 import com.example.myapplication.data.JokeGenerator
 import com.example.myapplication.databinding.ActivityJokesBinding
 import com.example.myapplication.databinding.FragmentJokesListBinding
+import com.example.myapplication.ui.fragments.FViewModel
 import com.example.myapplication.ui.fragments.JokesListFragment
 import com.example.myapplication.ui.joke_details.JokeDetailsActivity
 import com.example.myapplication.ui.joke_list.recycler.JokeAdapters.JokeAdapterForActivity
 
 class JokesActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityJokesBinding
-    private val bindingFragmentList: FragmentJokesListBinding by viewBinding(FragmentJokesListBinding::bind)
     private val adapter = JokeAdapterForActivity {
         startActivity(JokeDetailsActivity.getInstance(this, it))
     }
     private val jokeGenerator = JokeGenerator
+    private lateinit var binding: ActivityJokesBinding
+
+    private val viewModel: FViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //supportFragmentManager.fragmentFactory = CustomFragmentFactory()
