cfca45661eb60f344c9234a011296df484bd5b60
Max <max.lei@bk.ru>
2024-11-17 19:14:27 +0300

    правки

--- app/src/main/java/com/example/myapplication/ui/joke_list/JokesActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/joke_list/JokesActivity.kt
@@ -11,18 +11,11 @@ import com.example.myapplication.databinding.ActivityJokesBinding
 import com.example.myapplication.databinding.FragmentJokesListBinding
 import com.example.myapplication.ui.fragments.FViewModel
 import com.example.myapplication.ui.fragments.JokesListFragment
-import com.example.myapplication.ui.joke_details.JokeDetailsActivity
-import com.example.myapplication.ui.joke_list.recycler.JokeAdapters.JokeAdapterForActivity
 
 class JokesActivity : AppCompatActivity() {
 
-    private val adapter = JokeAdapterForActivity {
-        startActivity(JokeDetailsActivity.getInstance(this, it))
-    }
-    private val jokeGenerator = JokeGenerator
-    private lateinit var binding: ActivityJokesBinding
 
-    private val viewModel: FViewModel by viewModels()
+    private lateinit var binding: ActivityJokesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //supportFragmentManager.fragmentFactory = CustomFragmentFactory()
@@ -31,8 +24,6 @@ class JokesActivity : AppCompatActivity() {
         binding = ActivityJokesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        //createRWList(7)
         if (savedInstanceState == null) {
             openFragment()
         }
@@ -41,12 +32,6 @@ class JokesActivity : AppCompatActivity() {
     private fun openFragment() {
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container_view, JokesListFragment.newInstance())
-            .addToBackStack(null)
             .commit()
     }
-
-//    private fun createRWList(jokesCount: Int) {
-//        binding.rw.adapter = adapter
-//        adapter.setNewData(jokeGenerator.generateJokesList(jokesCount))
-//    }
 }
